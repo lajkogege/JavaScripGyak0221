c218d6336932f5e7f49cd9a213b780c206fe1545
lajkogege <lajkogege@gmail.com>
2024-02-21 13:34:12 +0100

    hatókörök

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
 import {paros}  from "./fugveny.js"
 
-console.log(paros(22))
+//console.log(paros(22))
+
+/*var let con--hatókötök */
+//console.log(szam); //szam--> hibaüzennet nem fut tovább a program, initialiton
+let szam = 12;
+
+function fv1(){
+    let szam =5;
+    console.log(szam);
+    let masikszam = 23;
+    console.log(masikszam);
+}
+
+//fv1()
+
+//console.log(masikszam); //hibaüzenet -- reference error, kizárolag csak a fügvényben értelmezhető
+
+console.log(szam);
+
+function fv2(){
+    let x =2;
+    {
+        let x =3;
+        console.log(x)
+    }
+    console.log(x)
+    // let=4 -->nem engedi ujra deklalárni
+}
+
+fv2()
+
+function fv3(){
+    for (let index = 0; index < 3; index++) {
+        console.log(index)
+    }
+    console.log(index)
+}
+
+fv3()
