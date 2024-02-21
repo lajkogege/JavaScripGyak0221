fba68bb9764884f4dd47244cc47e17267817f236
lajkogege <lajkogege@gmail.com>
2024-02-21 13:02:50 +0100

    paros e fugveny

---
+++ fugveny.js
@@ -0,0 +1,10 @@
+
+export function paros(szam){
+    let vanparos =false;
+    if (szam % 2===0){
+       vanparos =true;
+    } else {
+        vanparos=false;
+    }
+    return vanparos
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+import {paros}  from "./fugveny.js"
+
+console.log(paros(22))
